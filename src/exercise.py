7d622896f5ba39c61b062f62faa0c16559615477
cabosh <callum.bosher@gmail.com>
2020-05-15 15:58:38 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    speed = int(input("Enter your speed: "))
+    if speed > 70:
+        print("Speeding ticket!")
+    else:
+        print("No speeding ticket")
 
 if __name__ == '__main__':
     main()
